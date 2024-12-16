71ed9ada79ce5a2be4b068b59e83cd4639f91571
Yevhen Horbachov <yevhen@Yevhens-MacBook-Pro.local>
2024-12-16 18:42:00 +0100

    chore: initial commit

---
+++ lib/tailwind_artisan.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tailwind_artisan/version"
+
+module TailwindArtisan
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tailwind_artisan/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TailwindArtisan
+  VERSION = "0.1.0"
+end
